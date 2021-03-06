76e6b6ebec0c0b22fc8eefdd4090a15827a3cb3d
Chad Gauthier <chadgauth@gmail.com>
2021-03-06 17:19:11 -0600

    working site

---
+++ src/atoms/Button.tsx
@@ -0,0 +1,45 @@
+import React from "react";
+import "./button.css";
+
+export interface ButtonProps {
+  /**
+   * Is this the principal call to action on the page?
+   */
+  primary?: boolean;
+  /**
+   * What background color to use
+   */
+  backgroundColor?: string;
+  /**
+   * How large should the button be?
+   */
+  label: string;
+  /**
+   * Optional click handler
+   */
+  onClick?: () => void;
+}
+
+/**
+ * Primary UI component for user interaction
+ */
+export const Button: React.FC<ButtonProps> = ({
+  primary = false,
+  backgroundColor,
+  label,
+  ...props
+}: ButtonProps) => {
+  const mode = primary
+    ? "storybook-button--primary"
+    : "storybook-button--secondary";
+  return (
+    <button
+      type="button"
+      className="hex-button"
+      style={{ backgroundColor }}
+      {...props}
+    >
+      {label}
+    </button>
+  );
+};
--- src/components/Header.stories.tsx
+++ src/components/Header.stories.tsx
@@ -1,11 +1,11 @@
 import React from "react";
-// also exported from '@storybook/react' if you can deal with breaking changes in 6.1
+
 import { Story, Meta } from "@storybook/react";
 
 import { Header, HeaderProps } from "./Header";
 
 export default {
-  title: "Example/Header",
+  title: "Components/Header",
   component: Header,
 } as Meta;
 
